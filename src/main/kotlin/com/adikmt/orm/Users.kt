a8c5ab53a78ef6fc21576461a04652d90ddf67f7
Adithya Kamath <kamathis4@gmail.com>
2023-03-15 16:03:40 +0530

    User Entity

---
+++ src/main/kotlin/com/adikmt/orm/Users.kt
@@ -0,0 +1,21 @@
+package com.adikmt.orm
+
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+import java.util.*
+
+object UserEntity : UUIDTable(name = "users") {
+    val email = varchar(name = "user_email", length = 100).uniqueIndex()
+    val username = varchar(name = "user_name", length = 30).uniqueIndex()
+    val password = varchar(name = "user_password", length = 50)
+    val bio = text(name = "user_bio").default(defaultValue = "")
+}
+
+object FollowersEntity : Table("users_follow") {
+    val userId: Column<EntityID<UUID>> =
+        reference(name = "user_id", foreign = UserEntity, onDelete = ReferenceOption.CASCADE)
+    val followeeId = reference(name = "followee_id", foreign = UserEntity, onDelete = ReferenceOption.CASCADE)
+}
