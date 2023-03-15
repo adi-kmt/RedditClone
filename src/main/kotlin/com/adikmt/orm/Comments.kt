918297832d7b033343908c716f568253cf64153a
Adithya Kamath <kamathis4@gmail.com>
2023-03-15 18:10:27 +0530

    Comment Entity creation, small fix

---
+++ src/main/kotlin/com/adikmt/orm/Comments.kt
@@ -0,0 +1,25 @@
+package com.adikmt.orm
+
+import com.adikmt.orm.PostEntity.defaultExpression
+import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
+
+
+//TODO Add nested comments
+object CommentEntity : UUIDTable(name = "comments") {
+    val text = text("comment_text")
+    val createdAt = datetime("post_datetime").defaultExpression(CurrentDateTime)
+    val post = reference("comment_post", PostEntity)
+    val author = reference("comment_author", UserEntity)
+//    val parentComment = reference("parent_comment", CommentEntity).nullable()
+}
+
+object CommentFavouriteEntity : Table(name = "comment_user_fav") {
+    val userId = reference("user_id", UserEntity, onDelete = ReferenceOption.CASCADE)
+    val commentId = reference("comment_id", CommentEntity, onDelete = ReferenceOption.CASCADE)
+
+    override val primaryKey: PrimaryKey = PrimaryKey(userId, commentId)
+}
