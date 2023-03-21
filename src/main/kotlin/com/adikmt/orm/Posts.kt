23826c62c6b20c7142fb2e3ab10e016059526bdc
Adithya Kamath <kamathis4@gmail.com>
2023-03-21 14:27:19 +0530

    Fixing Entities

--- src/main/kotlin/com/adikmt/orm/Posts.kt
+++ src/main/kotlin/com/adikmt/orm/Posts.kt
@@ -1,18 +1,17 @@
 package com.adikmt.orm
 
-import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 
-object PostEntity: UUIDTable(name = "posts"){
+object PostEntity : LongIdTable(name = "posts") {
     val title = varchar(name = "post_title", length = 255)
     val desc = text(name = "post_desc").nullable()
     val author = reference("post_author", UserEntity)
     val subreddit = reference("post_subreddit", SubredditEntity)
     val createdAt = datetime("post_datetime").defaultExpression(CurrentDateTime)
-
 }
 
 object PostFavouriteEntity: Table(name = "post_user_fav"){
