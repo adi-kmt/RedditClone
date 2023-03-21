287d207c71aa3fe7d38d4badd90c648c38d9aac4
Adithya Kamath <kamathis4@gmail.com>
2023-03-21 22:41:59 +0530

    Minor Entities update

--- src/main/kotlin/com/adikmt/orm/Comments.kt
+++ src/main/kotlin/com/adikmt/orm/Comments.kt
@@ -8,10 +8,10 @@ import org.jetbrains.exposed.sql.javatime.datetime
 
 object CommentEntity : LongIdTable(name = "comments") {
     val text = text("comment_text")
-    val createdAt = datetime("post_datetime").defaultExpression(CurrentDateTime)
+    val createdAt = datetime("comment_datetime").defaultExpression(CurrentDateTime)
     val post = reference("comment_post", PostEntity)
     val author = reference("comment_author", UserEntity)
-    val parentComment = reference("parent_comment", CommentEntity).nullable()
+    val parentComment = reference("parent_comment_id", CommentEntity).nullable()
 }
 
 object CommentFavouriteEntity : Table(name = "comment_user_fav") {
